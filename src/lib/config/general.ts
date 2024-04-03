b5e4a2c0a1ddc351ca7a971ec4c769328d738d13
Eyad Hasan <eyadgithub@proton.me>
2024-04-03 17:53:26 -0400

    Updating configs, personalizing the site

--- src/lib/config/general.ts
+++ src/lib/config/general.ts
@@ -56,12 +56,12 @@ export const head: HeadConfig = {}
 export const header: HeaderConfig = {
   nav: [
     {
-      text: 'Get Started',
-      link: '/hello-world'
+      text: 'Projects',
+      link: '/projects'
     },
     {
-      text: 'Elements',
-      link: '/hello-world/elements'
+      text: 'Resume',
+      link: '/resume'
     }
   ]
 }
--- src/lib/config/site.ts
+++ src/lib/config/site.ts
@@ -3,15 +3,15 @@ import type { SiteConfig } from '$lib/types/site'
 export const site: SiteConfig = {
   protocol: import.meta.env.URARA_SITE_PROTOCOL ?? import.meta.env.DEV ? 'http://' : 'https://',
   domain: import.meta.env.URARA_SITE_DOMAIN ?? 'urara-demo.netlify.app',
-  title: 'Urara',
-  subtitle: 'Sweet & Powerful SvelteKit Blog Template',
+  title: 'iameyad',
+  subtitle: 'Another devblog',
   lang: 'en-US',
-  description: 'Powered by SvelteKit/Urara',
+  description: 'Powered by a lack of sleep',
   author: {
     avatar: '/assets/maskable@512.png',
-    name: 'John Doe',
-    status: '🌸',
-    bio: 'lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+    name: 'Eyad Hasan',
+    status: '',
+    bio: 'Powerlifting DevOps Engineer.'
   },
   themeColor: '#3D4451'
 }
