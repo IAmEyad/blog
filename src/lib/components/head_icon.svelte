da09f09f3c8c14e7a65baed8012bf9f54f43ba12
Eyad Hasan <eyadgithub@proton.me>
2024-04-03 21:16:44 -0400

    Not sure why it's failing to build :|

--- src/lib/components/head_icon.svelte
+++ src/lib/components/head_icon.svelte
@@ -7,7 +7,7 @@
     <link rel="shortcut icon" href={favicon.src} sizes={favicon.sizes} type={favicon.type} />
   {/if}
   {#if any['180']}
-    <link rel="apple-touch-icon" href={any['128'].src} sizes={any['128'].sizes} type={any['128'].type} />
+    <link rel="apple-touch-icon" href={any['180'].src} sizes={any['180'].sizes} type={any['180'].type} />
   {/if}
   {#if any['192']}
     <link rel="icon" href={any['192'].src} sizes={any['192'].sizes} type={any['192'].type} />
