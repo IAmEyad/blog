a23365f56edd7ddb50688e6b4aa8f2bc628b5318
Eyad Hasan <eyadgithub@proton.me>
2024-04-03 22:01:38 -0400

    Seeing if I just need to force some values

--- src/lib/config/site.ts
+++ src/lib/config/site.ts
@@ -2,16 +2,16 @@ import type { SiteConfig } from '$lib/types/site'
 
 export const site: SiteConfig = {
   protocol: import.meta.env.URARA_SITE_PROTOCOL ?? import.meta.env.DEV ? 'http://' : 'https://',
-  domain: import.meta.env.URARA_SITE_DOMAIN,
+  domain: 'iameyad.dev',
   title: 'iameyad',
   subtitle: 'Another devblog',
   lang: 'en-US',
-  description: 'Powered by a lack of sleep',
+  description: 'Another devblog',
   author: {
     avatar: '/assets/maskable@512.png',
     name: 'Eyad Hasan',
     status: '',
-    bio: 'Powerlifting DevOps Engineer.',
+    bio: 'DevOps Engineer.  Powerlifter.  Video Game enjoyer.',
     metadata: [
       {
         icon: 'i-heroicons-solid-code-bracket-square',
